cf6f26be28e4885d9f91bd67ca108937e76a4583
Morphine <64981166+hellvn@users.noreply.github.com>
2021-06-14 21:07:40 +0700

    sấ

---
+++ UWP-Practical-1/Services/ReadFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Newtonsoft.Json;
+
+namespace UWP_Practical_1.Services
+{
+    class ReadFile
+    {
+        public async Task<Models.Employee> ReadJson()
+        {
+            var storage = ApplicationData.Current.LocalFolder;
+            var EmpJson = await storage.CreateFileAsync("employee.json", CreationCollisionOption.OpenIfExists);
+            var Jsontext = await FileIO.ReadTextAsync(EmpJson);
+            Models.Employee employee = JsonConvert.DeserializeObject<Models.Employee>(Jsontext);
+            return employee;
+         }
+    }
+}

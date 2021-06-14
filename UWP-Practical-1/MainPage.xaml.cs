4d9efe09f27a7e02a850c2627e665e202dc6a232
hellvn <vuongpt93@gmail.com>
2021-06-15 03:45:56 +0700

    2

--- UWP-Practical-1/MainPage.xaml.cs
+++ UWP-Practical-1/MainPage.xaml.cs
@@ -1,21 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Newtonsoft.Json;
-using System.Threading.Tasks;
-using UWP_Practical_1.Models;
+﻿using UWP_Practical_1.Models;
 using UWP_Practical_1.Services;
+using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,22 +11,24 @@ namespace UWP_Practical_1
     /// </summary>
     public sealed partial class MainPage : Page
     {
-       
+
         public MainPage()
         {
             this.InitializeComponent();
+            Write_file();
+            getEMP();
         }
-        private async void getEMP(object sender, RoutedEventArgs e)
+        private async void getEMP()
         {
             ReadFile readFile = new ReadFile();
             Employee employee = await readFile.ReadJson("employee.json");
-            if(employee != null)
+            if (employee != null)
             {
                 EmpList.ItemsSource = employee.employee_list;
             }
 
         }
-        private void Write_file(object sender, RoutedEventArgs e)
+        private void Write_file()
         {
             string json = @"{
             'employee_list': [
@@ -65,6 +52,6 @@ namespace UWP_Practical_1
             ReadFile.WriteFile("employee.json", json);
         }
 
-       
+
     }
 }
